8c60484e5ca16b33d3583f49d291fd6dcbbff09a
Fabien Laurette <fabien.laurette@gmail.com>
2021-02-14 23:06:07 +0100

    TCL2-v1

---
+++ tcl-domain/src/main/java/fr/epita/filrouge/domain/video/Video.java
@@ -0,0 +1,59 @@
+package fr.epita.filrouge.domain.video;
+
+public abstract class Video {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private Double averageRating;
+    private Integer numberOfVotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public Integer getNumberOfVotes() {
+        return numberOfVotes;
+    }
+
+    public void setNumberOfVotes(Integer numberOfVotes) {
+        this.numberOfVotes = numberOfVotes;
+    }
+}
