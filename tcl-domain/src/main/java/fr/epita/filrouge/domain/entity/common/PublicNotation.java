f8a1e739255d421c0f16fff856d8dc84d13a801c
Fabien Laurette <fabien.laurette@gmail.com>
2021-02-23 11:48:29 +0100

    corrections tcl006-copie

--- tcl-domain/src/main/java/fr/epita/filrouge/domain/entity/common/PublicNotation.java
+++ tcl-domain/src/main/java/fr/epita/filrouge/domain/entity/common/PublicNotation.java
@@ -3,4 +3,21 @@ package fr.epita.filrouge.domain.entity.common;
 public class PublicNotation {
     private Double averageRating;
     private Integer nombreOfVotes;
+
+    public Double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public Integer getNombreOfVotes() {
+        return nombreOfVotes;
+    }
+
+    public void setNombreOfVotes(Integer nombreOfVotes) {
+        this.nombreOfVotes = nombreOfVotes;
+    }
+
 }
--- tcl-exposition/src/main/java/fr/epita/filrouge/FilrougeApplication.java
+++ tcl-exposition/src/main/java/fr/epita/filrouge/FilrougeApplication.java
@@ -1,31 +1,10 @@
 package fr.epita.filrouge;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@SpringBootApplication(scanBasePackages = {"fr.epita.filrouge"})
-
-
-@EnableSwagger2
-@EnableJpaRepositories(basePackages = {"fr.epita.filrouge"})
-@EntityScan("fr.epita.filrouge")
+@SpringBootApplication
 public class FilrougeApplication {
-
-    private static final Logger LOG = LoggerFactory.getLogger(FilrougeApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(FilrougeApplication.class, args);
-
-        LOG.info("Let's start ! ");
-        LOG.info("swagger link : http://localhost:8080/swagger-ui.html");
-
-
+        System.out.println("Let's start ! ");
     }
-
 }
