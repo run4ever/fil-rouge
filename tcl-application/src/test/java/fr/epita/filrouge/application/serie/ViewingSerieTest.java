c3422686715cbb24f530c2fc7dbe2ebd198429c6
Yoss <youssrabou@gmail.com>
2021-02-27 22:38:53 +0100

    viewving_appli_expo

--- tcl-application/src/test/java/fr/epita/filrouge/application/serie/ViewingSerieTest.java
+++ tcl-application/src/test/java/fr/epita/filrouge/application/serie/ViewingSerieTest.java
@@ -48,49 +48,49 @@ public class ViewingSerieTest {
 
 
 
-
-    @Test
-    @DisplayName("tester la bonne création d'une série")
-    public void createSerie_should_success() {
-
-
-        //Given
-        //Given
-        SerieDto serieDto = new SerieDto ();
-        serieDto.setCategory (Category.COMEDY);
-        serieDto.setImdbId ("tt00021");
-        serieDto.setDescription ("Prison Break");
-        serieDto.setNumberOfEpisode (20);
-        serieDto.setNumberOfSeason (5);
-        serieDto.setStartYear (1963);
-        serieDto.setEndYear (1969);
-
-        Serie serieMock = new Serie ();
-        serieMock.setCategory (Category.COMEDY);
-        serieMock.setImdbId ("tt00021");
-        serieMock.setDescription ("Prison Break");
-        serieMock.setNumberOfEpisode (20);
-        serieMock.setNumberOfSeason (5);
-        serieMock.setStartYear (1963);
-        serieMock.setEndYear (1969);
-
-        AppUser appUser = AppUser.Builder.anAppUser()
-                .withFirstname("Jean")
-                .withLastname("Durant")
-                .withEmail("yoss@test.com")
-                .withBirthdayDate(LocalDate.of(1950, 12, 25))
-                .withRole(Role.ROLE_RESP)
-                .withPassword("testpass")
-                .build();
-
-        ViewingSerieCreateDto viewingSerieDto = new ViewingSerieCreateDto ();
-        viewingSerieDto.setCurrentEpisode (1);
-        viewingSerieDto.setCurrentSeason (1);
-        viewingSerieDto.setEmail ("yoss@test.com");
-        viewingSerieDto.setStatus (TO_WATCH);
-
-
-
-    }
+//
+//    @Test
+//    @DisplayName("tester la bonne création d'une série")
+//    public void createSerie_should_success() {
+//
+//
+//        //Given
+//        //Given
+//        SerieDto serieDto = new SerieDto ();
+//        serieDto.setCategory (Category.COMEDY);
+//        serieDto.setImdbId ("tt00021");
+//        serieDto.setDescription ("Prison Break");
+//        serieDto.setNumberOfEpisode (20);
+//        serieDto.setNumberOfSeason (5);
+//        serieDto.setStartYear (1963);
+//        serieDto.setEndYear (1969);
+//
+//        Serie serieMock = new Serie ();
+//        serieMock.setCategory (Category.COMEDY);
+//        serieMock.setImdbId ("tt00021");
+//        serieMock.setDescription ("Prison Break");
+//        serieMock.setNumberOfEpisode (20);
+//        serieMock.setNumberOfSeason (5);
+//        serieMock.setStartYear (1963);
+//        serieMock.setEndYear (1969);
+//
+//        AppUser appUser = AppUser.Builder.anAppUser()
+//                .withFirstname("Jean")
+//                .withLastname("Durant")
+//                .withEmail("yoss@test.com")
+//                .withBirthdayDate(LocalDate.of(1950, 12, 25))
+//                .withRole(Role.ROLE_RESP)
+//                .withPassword("testpass")
+//                .build();
+//
+//        ViewingSerieCreateDto  viewingSerieDto = new ViewingSerieCreateDto ();
+//        viewingSerieDto.setCurrentEpisode (1);
+//        viewingSerieDto.setCurrentSeason (1);
+//        viewingSerieDto.setEmail ("yoss@test.com");
+//        viewingSerieDto.setStatus (TO_WATCH);
+//
+//
+//
+//    }
 
 }
