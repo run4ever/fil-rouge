e02afdb57bcbe1b75d614feebcc80813ce7867c2
dragonoir518 <arnaudchenier@gmail.com>
2021-03-08 09:58:13 +0100

    ajout allowedHeaders dans controller

--- tcl-exposition/src/main/java/fr/epita/filrouge/exposition/controller/SerieResource.java
+++ tcl-exposition/src/main/java/fr/epita/filrouge/exposition/controller/SerieResource.java
@@ -20,7 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins="http://localhost:4200", allowedHeaders = "*")
 @Validated
 @RestController
 @RequestMapping("/api/v1/serie")
